8c0aa831445d6736cdcaa28fdd589447a3121aa1
Ivan Pronin <pacernestit@rambler.ru>
2024-11-24 19:20:26 +0300

    frontend fix (add main routes)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/home/homePage";
+import "./App.css";
+import { Layout } from "./layout/layout";
+import { AboutPage } from "./pages/about/aboutPage";
+import { PersonPage } from "./pages/person/personPage";
+import { RegisterPage } from "./pages/authorization/register/register";
 
 function App() {
   return (
-    <>
+    <div>
       <Routes>
-        <Route path='/' element=''></Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="home" element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="person" element={<PersonPage />} />
+        </Route>
+
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
