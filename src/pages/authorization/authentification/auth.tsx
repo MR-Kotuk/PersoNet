a5bdeb229e467c9740c693d733121ac75ec9082e
Ivan Pronin <pacernestit@rambler.ru>
2024-12-15 20:42:31 +0300

    frontend feat (add datapost at auth page)

--- src/pages/authorization/authentification/auth.tsx
+++ src/pages/authorization/authentification/auth.tsx
@@ -1,8 +1,44 @@
-import { FC } from "react";
+import { ChangeEvent, FC, FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./auth.css";
 
 export const AuthPage: FC = () => {
+  interface IAuthData {
+    email: string;
+    password: string;
+  }
+
+  const [authData, setAuthData] = useState<IAuthData>({
+    email: "",
+    password: "",
+  });
+
+  const setAuthDataInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setAuthData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const postAuthData = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/auth/login",
+        authData
+      );
+
+      const token = response.data;
+      localStorage.setItem("token", token);
+    } catch (e) {
+      alert("Invalid data");
+    }
+  };
+
   return (
     <div className="auth-container">
       <div className="auth-greeting-wrapper">
@@ -20,26 +56,37 @@ export const AuthPage: FC = () => {
           Perso <span>|||</span> et
         </p>
 
-        <p className="auth-field-text">Create account</p>
+        <p className="auth-field-text">Log in</p>
 
-        <form className="auth-field">
+        <form className="auth-field" onSubmit={postAuthData}>
           <div className="auth-field-item">
             <p>Email</p>
-            <input type="email" name="email" placeholder="Enter your email" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              onChange={setAuthDataInput}
+            />
           </div>
 
           <div className="auth-field-item">
             <p>Password</p>
-            <input type="password" placeholder="Enter your password" />
+            <input
+              type="password"
+              placeholder="Enter your password"
+              onChange={setAuthDataInput}
+            />
           </div>
 
           <p className="auth-forgot-password-item">Forgot password?</p>
 
           <div className="auth-field-item">
-            <button className="auth-submit">Submit</button>
+            <button className="auth-submit" type="submit">
+              Submit
+            </button>
 
             <p className="auth-enter-account">
-              Already have no account?{" "}
+              Have no account?{" "}
               <Link to="*" className="auth-enter-account-link">
                 Create
               </Link>
